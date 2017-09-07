d71abdaf758844b6df6c13bd56f82a7b6e75f113
equation314 <equation618@gmail.com>
2017-09-08 01:08:55 +0800

    Add basic UI

---
+++ app/src/main/java/com/java/g39/main/MainActivity.java
@@ -0,0 +1,141 @@
+package com.java.g39.main;
+
+import android.content.Intent;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.java.g39.R;
+import com.java.g39.newslist.NewsListFragment;
+
+/**
+ * Created by equation on 9/7/17.
+ */
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, MainContract.View {
+
+    private Toolbar mToolbar;
+    private MainContract.Presenter mPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mPresenter = new MainPresenter(this);
+
+        this.switchToNews();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        mPresenter.switchNavigation(item.getItemId());
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void setPresenter(MainContract.Presenter presenter) {
+        this.mPresenter = presenter;
+    }
+
+    @Override
+    public Context context() {
+        return null;
+    }
+
+    @Override
+    public void start(Intent intent) {
+    }
+
+    @Override
+    public void switchToNews() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, NewsListFragment.newInstance()).commit();
+        this.mToolbar.setTitle(R.string.nav_news_title);
+    }
+
+    @Override
+    public void switchToFavorites() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, NewsListFragment.newInstance()).commit();
+        this.mToolbar.setTitle(R.string.nav_favorites_title);
+    }
+
+    @Override
+    public void switchToSettings() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, NewsListFragment.newInstance()).commit();
+        this.mToolbar.setTitle(R.string.nav_settings_title);
+    }
+
+    @Override
+    public void switchToAbout() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, NewsListFragment.newInstance()).commit();
+        this.mToolbar.setTitle(R.string.nav_about_title);
+    }
+}
---
+++ app/src/main/java/com/java/g39/main/MainContract.java
@@ -0,0 +1,44 @@
+package com.java.g39.main;
+
+import com.java.g39.BaseView;
+import com.java.g39.BasePresenter;
+
+/**
+ * Created by equation on 9/7/17.
+ */
+
+public interface MainContract {
+
+    interface View extends BaseView<MainContract.Presenter> {
+
+        /**
+         * 切换到新闻页面
+         */
+        void switchToNews();
+
+        /**
+         * 切换到收藏页面
+         */
+        void switchToFavorites();
+
+        /**
+         * 切换到设置页面
+         */
+        void switchToSettings();
+
+        /**
+         * 切换到关于页面
+         */
+        void switchToAbout();
+    }
+
+    interface Presenter extends BasePresenter {
+
+        /**
+         * 切换页面
+         *
+         * @param id 页面 ID
+         */
+        void switchNavigation(int id);
+    }
+}
