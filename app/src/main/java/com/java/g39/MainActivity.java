d15e71bd348d43ba6a63c2fc166e7e9f64ae51cb
ChenYu <believe.chenyu@gmail.com>
2017-09-07 17:12:15 +0800

    add rxjava

--- app/src/main/java/com/java/g39/MainActivity.java
+++ app/src/main/java/com/java/g39/MainActivity.java
@@ -2,6 +2,10 @@ package com.java.g39;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.*;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Flowable.just("Hello RxJava").subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.d("DEBUG", s);
+            }
+        });
     }
 }
